99fc2364c9aa397fdc43f4128ad781993bfa82ec
anthonypecchillo <anthonypecchillo@gmail.com>
2016-11-28 23:44:14 -0800

    Defined tweet structure and created an updateTweets function

--- twittler.js
+++ twittler.js
@@ -2,13 +2,41 @@ $(document).ready(function(){
   var $body = $('body');
   $body.html('');
 
-  var index = streams.home.length - 1;
-  while(index >= 0){
-    var tweet = streams.home[index];
-    var $tweet = $('<div></div>');
-    $tweet.text('@' + tweet.user + ': ' + tweet.message);
-    $tweet.appendTo($body);
-    index -= 1;
-  }
+  // Add a list to the body to append tweets to.
+  var $tweetList = $('<ul></ul>');
+  $tweetList.appendTo($body);
+
+  var updateTweets = function() {
+    $tweetList.html('');
+
+    var index = streams.home.length - 1;
+    while (index >= 0) {
+      var tweet = streams.home[index];
+      var $tweet = $(
+        '<li class="tweet">' +
+          
+          '<p class="tweet_heading">' + 
+            '<span class="user">' + tweet.user + ' </span>' + 
+            '<span class="tweeted_at">' + '@' + tweet.user + ' </span>' + 
+            '<span class="created_at">' + tweet.created_at + '</span>' +
+          '</p>' +
+
+          '<p class="message">' + tweet.message + '</p>' +
+  
+          '<button class="btn btn-primary">' + 'Reply Icon' + '</button>' +
+          '<button class="btn btn-success">' + 'Retweet Icon' + '</button>' +
+          '<button class="btn btn-info">' + 'Like Icon' + '</button>' +
+          '<button class="btn btn-warning">' + 'More Icon' + '</button>' +
+  
+        '</li>');
+       
+      $tweet.appendTo($tweetList);
+      index -= 1;
+    }
+  };
+
+  updateTweets();
+  setInterval(updateTweets, 1000);
+  
 
 });
