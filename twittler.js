1f7b3c3616638bec34fb10bdcab01c935a7a50f9
anthonypecchillo <anthonypecchillo@gmail.com>
2016-11-29 16:49:11 -0800

    Added functionality to refine the tweetList; show only a single user's tweets

--- twittler.js
+++ twittler.js
@@ -2,22 +2,39 @@ $(document).ready(function(){
   var $body = $('body');
   $body.html('');
 
+  // Default tweetList display to the home screen (all tweets).
+  var blargh = 'home';  
+
   // Add a list to the body to append tweets to.
   var $tweetList = $('<ul></ul>');
   $tweetList.appendTo($body);
 
-  var updateTweets = function() {
+  // Update tweets in tweetList
+  var index;
+  var updateTweets = function(currentUser) {
     $tweetList.html('');
-
-    var index = streams.home.length - 1;
+    
+    if (streams.users.hasOwnProperty(currentUser)) {
+      index = streams.users[currentUser].length - 1;
+    } else {
+      index = streams.home.length - 1;
+    }
+    
     while (index >= 0) {
-      var tweet = streams.home[index];
+      var tweet;
+      // tweet = currentUser === 'home' ? streams.home[index] : streams.users[currentUser][index];
+      if (currentUser === 'home') {
+        tweet = streams.home[index];
+      } else {
+        tweet = streams.users[currentUser][index];
+      }
+
       var $tweet = $(
         '<li class="tweet">' +
           
           '<p class="tweet_heading">' + 
-            '<span class="user">' + tweet.user + ' </span>' + 
-            '<span class="tweeted_at">' + '@' + tweet.user + ' </span>' + 
+            '<span class="user">' + tweet.user + '</span>' + 
+            '<span class="tweeted_at">' + ' @' + tweet.user + ' </span>' + 
             '<span class="created_at">' + tweet.created_at + '</span>' +
           '</p>' +
 
@@ -33,10 +50,17 @@ $(document).ready(function(){
       $tweet.appendTo($tweetList);
       index -= 1;
     }
+    
+    var $usernames = $('.user');
+    $usernames.click(refineTweetList);
   };
 
-  updateTweets();
-  setInterval(updateTweets, 1000);
-  
+  var refineTweetList = function() {
+    blargh = $(this).text();
+    updateTweets(blargh);
+  };
 
+  updateTweets(blargh);
+  setInterval(function() { updateTweets(blargh); }, 1000);
+  
 });
